9c6fd071270a8f2320d7245e10a67cbe1418d5ee
Eytan Adler <eytana@umich.edu>
2022-10-30 13:27:08 -0400

    It's working, now it needs to get much speedier

--- toasty/test_FEM_comp.py
+++ toasty/test_FEM_comp.py
@@ -4,29 +4,65 @@ import os
 import numpy as np
 from toasty.FEM_comp import FEM
 
-p = om.Problem()
-fem = p.model = FEM()
-mesh_x, mesh_y = fem.get_mesh()
-nx = mesh_x.shape[0]
-ny = mesh_x.shape[1]
+class Mass(om.ExplicitComponent):
+    def initialize(self):
+        self.options.declare("mesh_size", types=tuple, default=(2, 2), desc="Tuple of mesh size in (nx, ny)")
+    
+    def setup(self):
+        nx, ny = self.options["mesh_size"]
+        self.add_input("density", shape=((nx - 1) * (ny - 1),))
+        self.add_output("mass")
 
-p.model.nonlinear_solver = om.NewtonSolver(iprint=2, solve_subsystems=True)
+        self.declare_partials("mass", "density", val=1.0)
+    
+    def compute(self, inputs, outputs):
+        outputs["mass"] = np.sum(inputs["density"])
+
+nx = 31
+ny = 31
+x_linspace = np.linspace(0, 1.0, nx)
+y_linspace = np.linspace(0, 1.0, ny)
+mesh_x, mesh_y = np.meshgrid(x_linspace, y_linspace, indexing="ij")
+
+T_set = np.full((nx, ny), np.inf)
+T_set[:, 0] = 200.0
+
+q = np.zeros((nx - 1, ny - 1), dtype=float)
+q[0, -1] = 2e7
+q[nx // 2, -1] = 5e7
+q[-1, -1] = 2e7
+
+p = om.Problem()
+fem = p.model.add_subsystem("fem", FEM(mesh_x=mesh_x, mesh_y=mesh_y, T_set=T_set, q=q), promotes=["*"])
+p.model.add_subsystem("mass", Mass(mesh_size=(nx, ny)), promotes=["*"])
+
+p.model.nonlinear_solver = om.NewtonSolver(iprint=2, solve_subsystems=False, atol=1e-8)
 p.model.linear_solver = om.DirectSolver()
+
+p.model.add_objective("mass")
+p.model.add_design_var("density", lower=1e-6, upper=1.0)
+p.model.add_constraint("temp", upper=360)
+
+p.driver = om.pyOptSparseDriver(optimizer='SNOPT')
+p.driver.opt_settings['Major iterations limit'] = 20
+p.driver.opt_settings['Major optimality tolerance'] = 1e-6
+p.driver.opt_settings['Major feasibility tolerance'] = 1e-8
+# p.driver.opt_settings["Verify level"] = 3
 
 p.setup()
 
-q = np.zeros((nx - 1, ny - 1), dtype=float)
-q[nx // 2, ny - 2] = 20.0 * nx * ny
-q[-2, nx // 3 : 2 * nx // 3] = -2e5
-fem.set_element_heat(q)
+# om.n2(p)
+
 density = np.ones((nx - 1, ny - 1))
-density[np.ix_(np.arange(nx // 3, 2 * nx // 3), np.arange(ny // 6, 5 * ny // 6))] = 0.00001
-density[np.ix_(np.arange(0, nx // 3), np.arange(ny // 6, 5 * ny // 6))] = 0.2
 p.set_val("density", density.flatten())
 
-p.run_model()
+# p.check_partials()
+# p.run_model()
+p.run_driver()
 
 T = p.get_val("temp").reshape(nx, ny)
+density = p.get_val("density").reshape(nx - 1, ny - 1)
+print(T)
 
 fig, axs = plt.subplots(2, 1, figsize=(5, 8))
 c = axs[0].contourf(mesh_x, mesh_y, T, 100, cmap="coolwarm")
