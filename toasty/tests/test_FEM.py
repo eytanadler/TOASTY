69cfaa5a08a3566d94df4bce6dc9a06c4a11871f
eytanadler <eytana@umich.edu>
2022-11-02 14:40:40 -0400

    Added more finite element tests

--- toasty/tests/test_FEM.py
+++ toasty/tests/test_FEM.py
@@ -443,5 +443,117 @@ class NineElem(unittest.TestCase):
         om_assert.assert_check_partials(p.check_partials(), atol=1e-5, rtol=1e-8)
 
 
+class RectangularMesh(unittest.TestCase):
+    """
+    Test where there are a different number of nodes in the x and y directions.
+    """
+
+    def setUp(self):
+        self.rand = np.random.default_rng(95)  # random generator
+
+        self.nx = 3
+        self.ny = 4
+
+        self.T_set = np.full((self.nx, self.ny), np.inf)  # don't set any temperatures
+        self.q = np.zeros((self.nx - 1, self.ny - 1))
+
+        self.K_glob = np.array([[ 722.22222222, -388.88888889,    0.        ,    0.        ,
+                                    27.77777778, -361.11111111,    0.        ,    0.        ,
+                                    0.        ,    0.        ,    0.        ,    0.        ],
+                                [-388.88888889, 1444.44444444, -388.88888889,    0.        ,
+                                    -361.11111111,   55.55555556, -361.11111111,    0.        ,
+                                    0.        ,    0.        ,    0.        ,    0.        ],
+                                [   0.        , -388.88888889, 1444.44444444, -388.88888889,
+                                    0.        , -361.11111111,   55.55555556, -361.11111111,
+                                    0.        ,    0.        ,    0.        ,    0.        ],
+                                [   0.        ,    0.        , -388.88888889,  722.22222222,
+                                    0.        ,    0.        , -361.11111111,   27.77777778,
+                                    0.        ,    0.        ,    0.        ,    0.        ],
+                                [  27.77777778, -361.11111111,    0.        ,    0.        ,
+                                    1444.44444444, -777.77777778,    0.        ,    0.        ,
+                                    27.77777778, -361.11111111,    0.        ,    0.        ],
+                                [-361.11111111,   55.55555556, -361.11111111,    0.        ,
+                                    -777.77777778, 2888.88888889, -777.77777778,    0.        ,
+                                    -361.11111111,   55.55555556, -361.11111111,    0.        ],
+                                [   0.        , -361.11111111,   55.55555556, -361.11111111,
+                                    0.        , -777.77777778, 2888.88888889, -777.77777778,
+                                    0.        , -361.11111111,   55.55555556, -361.11111111],
+                                [   0.        ,    0.        , -361.11111111,   27.77777778,
+                                    0.        ,    0.        , -777.77777778, 1444.44444444,
+                                    0.        ,    0.        , -361.11111111,   27.77777778],
+                                [   0.        ,    0.        ,    0.        ,    0.        ,
+                                    27.77777778, -361.11111111,    0.        ,    0.        ,
+                                    722.22222222, -388.88888889,    0.        ,    0.        ],
+                                [   0.        ,    0.        ,    0.        ,    0.        ,
+                                    -361.11111111,   55.55555556, -361.11111111,    0.        ,
+                                    -388.88888889, 1444.44444444, -388.88888889,    0.        ],
+                                [   0.        ,    0.        ,    0.        ,    0.        ,
+                                    0.        , -361.11111111,   55.55555556, -361.11111111,
+                                    0.        , -388.88888889, 1444.44444444, -388.88888889],
+                                [   0.        ,    0.        ,    0.        ,    0.        ,
+                                    0.        ,    0.        , -361.11111111,   27.77777778,
+                                    0.        ,    0.        , -388.88888889,  722.22222222]])
+
+    def test_stiffness(self):
+        """
+        Regression test for 9-element stiffness matrix.
+        """
+        p = om.Problem()
+        fem = p.model = FEM(num_x=self.nx, num_y=self.ny, T_set=self.T_set, q=self.q)
+        p.setup()
+
+        np.testing.assert_allclose(self.K_glob, fem.K_glob.toarray())
+        np.testing.assert_allclose(0.0, fem.F_glob)
+
+    def test_stiffness_set_node(self):
+        """
+        Regression test for 9-element stiffness matrix.
+        """
+        self.T_set[1, 1] = 10.0
+        p = om.Problem()
+        fem = p.model = FEM(num_x=self.nx, num_y=self.ny, T_set=self.T_set, q=self.q)
+        p.setup()
+
+        K = self.K_glob
+        K[self.ny + 1, :] = 0.0
+        K[self.ny + 1, self.ny + 1] = 1.0
+
+        F = np.zeros(self.nx * self.ny)
+        F[self.ny + 1] = self.T_set[1, 1]
+
+        np.testing.assert_allclose(self.K_glob, fem.K_glob.toarray())
+        np.testing.assert_allclose(F, fem.F_glob.flatten())
+
+    def test_partials(self):
+        """
+        Test partial derivatives at a reasonable design point.
+        """
+        self.T_set[0, 3] = 1.0
+        self.q[1, 1] = 1e3
+
+        p = om.Problem()
+        p.model.add_subsystem("fem", FEM(num_x=self.nx, num_y=self.ny, T_set=self.T_set, q=self.q), promotes=["*"])
+        p.setup()
+        p.run_model()
+
+        om_assert.assert_check_partials(p.check_partials(), atol=5e-6, rtol=1e-7)
+
+    def test_partials_random_densities(self):
+        """
+        Test partial derivatives at a reasonable design point.
+        """
+        self.T_set[0, 3] = 1.0
+        self.q[1, 1] = 1e3
+
+        p = om.Problem()
+        p.model.add_subsystem("fem", FEM(num_x=self.nx, num_y=self.ny, T_set=self.T_set, q=self.q), promotes=["*"])
+        p.setup()
+
+        p.set_val("density", self.rand.random((self.nx - 1, self.ny - 1)).flatten())
+        p.run_model()
+
+        om_assert.assert_check_partials(p.check_partials(), atol=5e-6, rtol=1e-8)
+
+
 if __name__ == "__main__":
     unittest.main()
