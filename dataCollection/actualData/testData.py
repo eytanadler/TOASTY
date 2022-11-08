a4803023f0fb194fc9dcacce9534e922eb3131cb
Hannah Hajdik <hajdik@umich.edu>
2022-11-08 14:24:59 -0500

    only record an arrival if it's exited the runway

--- dataCollection/actualData/testData.py
+++ dataCollection/actualData/testData.py
@@ -8,12 +8,13 @@ def testCatch():
     catchDepartures(airport, 3600)
     catchArrivals(airport, 1000)
 
+
 def testExits():
     airport = SanDiego()
     exitCountA = countTotals("arrivals", airport)
     exitCountD = countTotals("departures", airport)
-    tabulateExits(airport, exitCountA)
-    tabulateExits(airport, exitCountD)
+    tabulateExits("Arrivals", airport, exitCountA)
+    tabulateExits("Departures", airport, exitCountD)
 
 
 testExits()
