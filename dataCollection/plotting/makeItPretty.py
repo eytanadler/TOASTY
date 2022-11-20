6f1900bc8abd6ed1d7b2272869c3595009c7c732
Hannah Hajdik <hajdik@umich.edu>
2022-11-20 13:51:28 -0500

    move things around for no reason

--- dataCollection/plotting/makeItPretty.py
+++ dataCollection/plotting/makeItPretty.py
@@ -1,93 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pickle as pkl
 from os import listdir
 from os.path import isfile, join
 import tilemapbase as tmb
 import niceplots as nice
 
+from dataCollection.plotting.plotUtils import extractTrail, openPickle, flightIDString
+
 
 tmb.init(create=True)
 t = tmb.tiles.build_OSM()
-
-
-def openPickle(fileName):
-    """
-    Opens pickle file containg details of flight
-
-    Parameters
-    ----------
-    fileName : string
-        file name to be opened
-
-    Returns
-    -------
-    flightDetails dictionary
-        dictionary of all details on flight
-    """
-    flightFile = open(fileName, "rb")
-    flightDetails = pkl.load(flightFile)
-    flightFile.close()
-
-    if not isinstance(flightDetails, dict):
-        flightDetails = None
-
-    return flightDetails
-
-
-def flightIDString(flightDetails):
-    """
-    Create a nice string for a flight plot title
-
-    Parameters
-    ----------
-    flightDetails : dict
-        dictionary of all details on flight
-
-    Returns
-    -------
-    string
-        nice string for a title or something
-    """
-    if flightDetails["identification"]["callsign"] is not None:
-        callsign = flightDetails["identification"]["callsign"]
-    else:
-        callsign = "Unknown"
-
-    if flightDetails["airport"]["origin"] is not None:
-        origin = flightDetails["airport"]["origin"]["code"]["iata"]
-    else:
-        origin = "Unknown"
-
-    if flightDetails["airport"]["destination"] is not None:
-        destination = flightDetails["airport"]["destination"]["code"]["iata"]
-    else:
-        destination = "Unknown"
-
-    return f"Flight {callsign} from {origin} to {destination}"
-
-
-def extractTrail(flightDetails):
-    """
-    Get the longitude and latitude from the flightDetails dict in the right order for plotting
-
-    Parameters
-    ----------
-    flightDetails : dict
-        dictionary of all details on flight
-
-    Returns
-    -------
-    numpy array (n, 2)
-        longitude & latitude trail info for a flight for plotting
-    """
-    trail = flightDetails["trail"]
-    latlong = np.zeros((len(trail), 2))
-
-    for i, point in enumerate(trail):
-        latlong[i, :] = [point["lng"], point["lat"]]
-
-    return latlong
 
 
 def plotMap(flightDetails, airport, show=False):
@@ -130,26 +52,6 @@ def plotMap(flightDetails, airport, show=False):
         plt.show()
     else:
         plt.savefig(f"{flightID}.png")
-
-
-def plotAllInFolder(path, airport):
-    """
-    plot all the flight files at a path to debug
-
-    Parameters
-    ----------
-    path : string
-        path to folder of files
-    airport : Airport class
-        airport being studied for this case
-    """
-    departureFiles = [f for f in listdir(path) if isfile(join(path, f))]
-
-    for fileName in departureFiles:
-        fullName = join(path, fileName)
-
-        flightDetails = openPickle(fullName)
-        plotMap(flightDetails, airport, show=True)
 
 
 def plotExitBoxes(airport, plotAll=False, exitList=None, show=False):
@@ -258,3 +160,23 @@ def plotHeatMap(airport, exitPercent, date, departures=True, show=False):
         plt.show()
     else:
         plt.savefig(f"exit_heatmap_{airport.code}_{key}_{date}.png")
+
+
+def plotAllInFolder(path, airport):
+    """
+    plot all the flight files at a path to debug
+
+    Parameters
+    ----------
+    path : string
+        path to folder of files
+    airport : Airport class
+        airport being studied for this case
+    """
+    departureFiles = [f for f in listdir(path) if isfile(join(path, f))]
+
+    for fileName in departureFiles:
+        fullName = join(path, fileName)
+
+        flightDetails = openPickle(fullName)
+        plotMap(flightDetails, airport, show=True)
