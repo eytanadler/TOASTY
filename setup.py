ead4a5ba20fd6310ca787462eb3989c93ef23c84
Hannah Hajdik <hajdik@umich.edu>
2022-11-23 10:15:00 -0500

    add fork of FlightRadarAPI to setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "matplotlib",
         "Pillow",
         "tilemapbase",
+        "FlightRadarAPI @ git+https://github.com/hajdik/FlightRadarAPI.git",
     ],
     extras_require={
         "test": ["pytest"],
